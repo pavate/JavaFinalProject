4ba8437e2fc4faf44cae172496fabe1d8f3b4869
pavate <pavatevinayak@gmail.com>
2019-07-22 18:30:45 -0400

    All pages running part 2

--- src/bookdatabase/EmployeeLogin/EmployeeLoginFXMLController.java
+++ src/bookdatabase/EmployeeLogin/EmployeeLoginFXMLController.java
@@ -13,7 +13,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -25,6 +27,10 @@ public class EmployeeLoginFXMLController implements Initializable {
 
     @FXML
     private Button btnEmployeeLogin;
+    @FXML
+    private TextField txtEmpID;
+    @FXML
+    private TextField txtEmpPass;
 
     /**
      * Initializes the controller class.
@@ -32,16 +38,30 @@ public class EmployeeLoginFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
          btnEmployeeLogin.setOnAction((ActionEvent event) -> {
+//             String usename =txtEmpID.getText();
+//             String password =txtEmpPass.getText();
+//            
              Stage stage =(Stage)btnEmployeeLogin.getScene().getWindow();
              try {
+                  if( txtEmpID.getText().equals("admin") &&  txtEmpPass.getText().equals("admin")){
                  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EmployeePageFXML.fxml"));
                  Parent root1 = (Parent) fxmlLoader.load();
                  stage.setTitle("2ndpage");
                  stage.setScene(new Scene(root1));
                  stage.show();
+                  }else{
+                      Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setX(500);
+                alert.setY(350);
+                alert.setTitle("Wrong Combination");
+                alert.setHeaderText("Password or Username is Wrong ");
+                alert.setContentText("Please correct and try again");
+                alert.showAndWait();
+                  }
              } catch (Exception e) {
                  System.out.println("not working");
              }
+             
        });
         // TODO
     }    
--- src/bookdatabase/UserLogin/UserloginFXMLController.java
+++ src/bookdatabase/UserLogin/UserloginFXMLController.java
@@ -7,9 +7,16 @@ package bookdatabase.UserLogin;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -20,12 +27,38 @@ public class UserloginFXMLController implements Initializable {
 
     @FXML
     private Button btnLoginStudent;
+    @FXML
+    private TextField txtStuID;
+    @FXML
+    private TextField txtStuPass;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+         btnLoginStudent.setOnAction((ActionEvent event) -> {
+             Stage stage =(Stage)btnLoginStudent.getScene().getWindow();
+             try {
+                 if( txtStuID.getText().equals("user") &&  txtStuPass.getText().equals("user")){
+                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StudentPageFXML.fxml"));
+                 Parent root1 = (Parent) fxmlLoader.load();
+                 stage.setTitle("2ndpage");
+                 stage.setScene(new Scene(root1));
+                 stage.show();
+                 }else{
+                      Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setX(500);
+                alert.setY(350);
+                alert.setTitle("Wrong Combination");
+                alert.setHeaderText("Password or Username is Wrong ");
+                alert.setContentText("Please correct and try again");
+                alert.showAndWait();
+                  }
+             } catch (Exception e) {
+                 System.out.println("not working");
+             }
+       });
         
         // TODO
     }    
